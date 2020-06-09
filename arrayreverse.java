568befbe503f854e2a08e0d4db91aeb52e34095a
amit2699 <amitchourasia263@gmail.com>
2020-06-09 12:16:47 +0530

    Array Reverse Code in Java

---
+++ arrayreverse.java
@@ -0,0 +1,49 @@
+package HackerrankPractice;
+
+import java.util.Scanner;
+
+public class ArrayReverse {
+
+	public static int[] arrayRevrse(int[] arr,int start,int end) {
+		int temp;
+		 while (start < end) 
+	        { 
+	            temp = arr[start];  
+	            arr[start] = arr[end]; 
+	            arr[end] = temp; 
+	            start++; 
+	            end--; 
+	        }
+		
+		return arr;
+		
+	}
+	
+	static void printArray(int arr[],  
+            int size) 
+{ 
+for (int i = 0; i < size; i++) 
+System.out.print(arr[i] + " "); 
+
+System.out.println(); 
+}  
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int start = 0; 
+		int end = n-1;
+		int [] arr = new int[n];
+        for(int i = 0; i < arr.length; i++){
+			arr[i] = s.nextInt();
+		}
+        
+        arrayRevrse(arr, start, end);
+        printArray(arr,n);
+		
+	}
+
+}
